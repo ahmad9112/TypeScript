84a71feebb2f8a2e5eacabc95abcfdaae7963323
Sayed Ahmad <saiyadahmad455@gmail.com>
2025-10-04 04:49:25 +0530

    ts

---
+++ L1/app.js
@@ -0,0 +1,20 @@
+//Number,Stereing,Boolean
+function add(num1, num2, printResult, someText) {
+    //    if(typeof num1==="number" && typeof num2==="number"){
+    //       return num1+num2;
+    //    }else{
+    //   return num1+num2;
+    //    }
+    if (printResult) {
+        console.log("".concat(someText, " ").concat(num1 + num2));
+    }
+    else {
+        return num1 + num2;
+    }
+}
+var n1 = 20; //number
+var n2 = 30; //number
+var printResult = true;
+var someText = "Some of two numbers is=";
+var ans = add(n1, n2, printResult, someText);
+//  console.log(ans); 
